3bab540004f4e183c3596d637c308fdb1ea9ada5
Nakamura Shogo <s.nakamura@earth.sint.co.jp>
2022-07-04 16:12:01 +0900

    add setup

--- src/utils/setup.sh
+++ src/utils/setup.sh
@@ -3,3 +3,4 @@ sudo apt upgrade -y
 sudo apt install python3-pip -y
 sudo pip3 install --upgrade pip
 git config --system alias.ss status
+sudo pip3 install git+https://aisia@dev.azure.com/aisia/aisia-ad/_git/aisia-ad-ai-sdk
